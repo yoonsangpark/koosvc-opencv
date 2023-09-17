892e75195b2f53d5ba8ee6d759e94cf4e3598b91
yoonsangpark <yoonsangpark@gmail.com>
2023-09-17 09:33:25 -0400

    use VideoCapture

--- koosvc-opencv.cpp
+++ koosvc-opencv.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 
+#include "opencv2/opencv.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
 
@@ -10,6 +11,8 @@ using namespace cv;
 int main(void)
 {
 	cout << "Built with OpenCV " << CV_VERSION << endl;
+
+	VideoCapture cap("sample.mp4");
 
 	return 0;
 }
