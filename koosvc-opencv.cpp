13991b2f5da96790a8b8dd40d6fb00b4e0b44dc9
yoonsangpark <yoonsangpark@gmail.com>
2024-04-07 00:10:41 +0900

    imread()

--- koosvc-opencv.cpp
+++ koosvc-opencv.cpp
@@ -9,8 +9,8 @@ using namespace std;
 using namespace cv;
 
 
-//#define src_name "/mnt/sd2/code/hdal/samples/koosvc-opencv/opencv-arch-01.png"
-#define src_name "/mnt/sd2/code/hdal/samples/koosvc-opencv/1920-1080-240405.mp4"
+#define src_name "/mnt/sd2/code/hdal/samples/koosvc-opencv/opencv-arch-01.png"
+//#define src_name "/mnt/sd2/code/hdal/samples/koosvc-opencv/1920-1080-240405.mp4"
 //#define src_name "/mnt/sd2/code/hdal/samples/koosvc-opencv/sample.avi"
 //#define src_name "/mnt/sd2/code/hdal/samples/koosvc-opencv/sample.mp4"
 //#define src_name "/mnt/sd2/code/hdal/samples/koosvc-opencv/stopwatch.avi"
@@ -18,6 +18,25 @@ using namespace cv;
 
 int main(void)
 {
+	Mat img = imread(src_name, IMREAD_COLOR);
+
+	CV_Assert(img.data);
+
+	cout << "dims : " << img.dims << endl;
+	cout << "rows : " << img.rows << endl;
+	cout << "cols : " << img.cols << endl;
+	cout << "size() : " << img.size() << endl;
+	cout << "total() : " << img.total() << endl;
+	cout << "elemSize() : " << img.elemSize() << endl;
+	cout << "elemSize1() : " << img.elemSize1() << endl;
+	cout << "type() : " << img.type() << endl;
+	cout << "step : " << img.step << endl;
+	cout << "step1() : " << img.step1() << endl;
+	
+	cout << "depth() : " << img.depth() << endl;
+	cout << "channels() : " << img.channels() << endl;
+
+#if 0	
 	Mat frame, edge;
 
 	cout << "Built with OpenCV " << CV_VERSION << endl;
@@ -44,6 +63,6 @@ int main(void)
 	}
 
 	cap.release();
-
+#endif
 	return 0;
 }
